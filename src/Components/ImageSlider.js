705b88e46f38835fcbd78c3bd032fad9de8a8e0a
Fernando Perez <ferpega03@gmail.com>
2023-12-15 17:00:38 -0600

    No se pq fregados no se centra el carusel

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,85 +1,33 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons';
+import React from 'react';
+import { Carousel } from 'antd';
+import image1 from '../Images/PAPEADORES2.jpg';
+import image2 from '../Images/VIOLAS.jpg';
+import image3 from '../Images/GOLDEN.jpg';
+import image4 from '../Images/CONEJOS.jpg';
+import image5 from '../Images/AVENGERS.jpg';
+import image6 from '../Images/CAÑONEROS.jpg';
 
+const images = [
+    {url: image1, title: "Los Papeadores, Campeones actuales"},
+    {url: image2, title: "Las Violas"},
+    {url: image3, title: "Golden Eagles"},
+    {url: image4, title: "Conejos B"},
+    {url: image5, title: "Los Avengers"},
+    {url: image6, title: "Cañoneros"}
+];
 
-const ImageSlider = ({images, pWidth}) => {
-    const [current, setCurrent] = useState(0);
-    const timer = useRef(null);
-
-    const sliderStyles = {
-        display: "flex",
-        justifyContent: "center",
-        height: "100%",
-        position: "relative",
-        margin: "0 auto",
-    };
-    
-    const slideStyles = {
-        width: "80%",  
-        height: "100%",
-        borderRadius: "10px",
-        backgroundPosition: "center",
-        backgroundSize: "100% 100%",
-    };
-
-    const slidesContanerStyles = {
-        display: "flex",
-        height: "100%",
-        transition: "transform ease-out 0.4s",
-    }
-
-    const goPrev = () => {
-        const isFirst = current === 0;
-        const newIndex = isFirst ? images.length - 1 : current - 1;
-        setCurrent(newIndex);
-    }
-
-    const goNext = useCallback(() => {
-        const isLast = current === images.length - 1;
-        const newIndex = isLast ? 0 : current + 1;
-        setCurrent(newIndex);
-    }, [current, images])
-
-    useEffect(() => {
-        if(timer.current){
-            clearInterval(timer.current);
-        }
-        timer.current = setInterval(goNext, 4000);
-        return () => {clearInterval(timer.current)};
-    }, [goNext])
-
-    const slidesOverflow = {
-        overflow: "hidden",
-        height: "100%",
-    };
-
-    const getSlidesContainerWidth = () => ({
-        ...slidesContanerStyles,
-        width: `${pWidth * images.length}vw`,
-        transform: `translateX(${-(pWidth * current)}vw)`
-    });
-
-    const getStylesBackground = (current) => ({
-        ...slideStyles,
-        backgroundImage: `url(${images[current].url})`,
-        width: `${pWidth}vw`
-    });
-
-    return(
-        <div style={sliderStyles}>
-            <DoubleLeftOutlined style={{fontSize: "2rem", color: "282c34", cursor: "pointer"}} onClick={goPrev} />
-            
-            <div style={slidesOverflow}>
-                <div style={getSlidesContainerWidth()}>
-                    {images.map((_, current) => (
-                        <div key={current} style={getStylesBackground(current)} />
-                        ))}
-                </div>
-            </div>
-            
-            <DoubleRightOutlined style={{fontSize: "2rem", color: "282c34", cursor: "pointer"}} onClick={goNext} />
-        </div>
-    )
+const ImageSlider = () => {
+  return (
+    <div style={{border: "1px solid red", display: 'flex', justifyContent: 'center'}}>
+      <Carousel autoplay dotPosition={'left'} style={{display: "block", marginLeft: "auto", marginRight: "auto"}}>
+        {images.map((image, index) => (
+          <div key={index} style={{display: 'flex', justifyContent: 'center'}}>
+            <img src={image.url} alt={image.title} className="carousel-Img" />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default ImageSlider;
