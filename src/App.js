761f4e82b196743f9cf775bd889ed29cb384c968
Fernando Perez <ferpega03@gmail.com>
2024-02-03 17:22:39 -0600

    Routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SiderTeams from './Components/SiderTeams';
 import Home from './Home';
 import Contacto from './Contacto';
 import AppHeader from './Components/Header';
+import SeasonsCards from './Components/SeasonCards';
 import TeamTable from './Components/TeamTable';
 import GeneralTable from './Components/GeneralTable';
 import { useParams } from 'react-router-dom';
@@ -19,26 +20,40 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="contacto" element={<Contacto />} />
-        <Route path="/estadisticas/*" element={ <div className='TeamsContainer'> <SiderTeams EquiposRoutes={EquiposRoutes}/> </div>} />
+        <Route path="/estadisticas" element={<SeasonsCards />} />
+        <Route path="/estadisticas/:season/*" element={<SeasonRoutes />} />
         <Route path="*" element={<FuncNotFound />} />
       </Routes>
     </Router>
   );
 }
 
-function EquiposRoutes() {
+function SeasonRoutes(){
+  let {season} = useParams();
+  return(
+    <div className='TeamsContainer'>
+      <SiderTeams season={season} />
+      <Routes>
+        <Route path="/*" element={<EquiposRoutes season={season} />} />
+        <Route path="*" element={<FuncNotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+function EquiposRoutes({ season }) {
   return (
     <Routes>
-      <Route path="/TABLA" element={<GeneralTable />} />
-      <Route path=":teamName" element={<Team />} />
+      <Route path="TABLA" element={<GeneralTable season={season} />} />
+      <Route path=":teamName" element={<Team season={season} />} />
       <Route path="*" element={<FuncNotFound />} />
     </Routes>
   );
 }
 
-function Team(){
+function Team({ season }){
   let {teamName} = useParams();
-  return <TeamTable teamName={teamName} />
+  return <TeamTable teamName={teamName} season={season} />
 }
 
 export default App;
